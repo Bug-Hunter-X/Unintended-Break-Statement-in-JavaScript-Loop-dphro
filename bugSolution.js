a07f168dd1b197eacd76fb963b76cbd985cead8e
BugHunter-X <askzenai@gmail.com>
2025-01-19 03:04:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+function myFunc() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+}
+//Alternatively, using a while loop with a conditional check:
+function myFuncWhile() {
+  let i = 0;
+  while (i < 10) {
+    console.log(i);
+    i++;
+  }
+}
